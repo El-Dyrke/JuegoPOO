9d8287c1fd12e9e9b9cb8e7b488c7aee5dc0ff39
kalouseckJP <juanpablo.rodriguez@alumnos.ulagos.cl>
2021-11-28 15:21:13 -0300

    No se que estoy haciendo probablemnte explote todo

--- Juego POO/main.py
+++ Juego POO/main.py
@@ -1,22 +1,60 @@
 from Jugador import Jugador
 from Mapa import Mapa
 from Partida import Partida
-from Proyectil import Proyectil
+#from Proyectil import Proyectil
+import pygame
+
+pygame.init()
 
 if __name__ == "__main__":
-    Doctor=Jugador
-    print("\nLos movimientos del jugador son:\na: Izquierda\nd: Derecha\nw: Arriba\ns: Abajo\nc: Disparo\nPresione cualquier otra tecla para salir.")
-    while True:
-        Mov=input()
-        if Mov=="a" or Mov=="A":
-            print("El jugador se mueve a la izquierda")
-        elif Mov=="d" or Mov=="D":
-            print("El jugador se mueve a la derecha ")
-        elif Mov=="w" or Mov=="W":
-            print("El jugador se mueve hacia arriba")
-        elif Mov=="s" or Mov=="S":
-            print("El jugador se mueve hacia abajo")
-        elif Mov=="c" or Mov=="C":
-            print("El jugador dispara")
-        else:
-            break
+    
+     # ----------- Declarar constantes -------------------------
+    ventana_x = 1050
+    ventana_y = 650
+    ventana = pygame.display.set_mode((ventana_x,ventana_y))
+    pygame.display.set_caption("POO")
+    reloj = pygame.time.Clock()
+    negro = (0,0,0)
+    fuente=pygame.font.SysFont("segoe print", 22)
+    puntaje = 0
+
+    # ----------------  Función para repintar el cuadro de juego -----------------
+    def refresh():
+        ventana.fill((107,171,242))
+        Dr.dibujar(ventana)
+        pygame.display.update()
+
+    # Variable que controla la repeticion del juego completo con todas sus pantallas
+    repetir = True 
+
+    # ------------------------- Ciclo de repeticion de todo el juego ---------------------------
+    while repetir:
+
+	    # Inicializacion de elementos del juego
+        Dr = Jugador(int(0), int(0), "img/Dr/", ventana_x)
+        
+       
+        
+
+        esta_jugando=True
+        while esta_jugando:
+
+            ventana.fill((0,0,0))
+
+            # Control de velocidad del juego
+            reloj.tick(27)
+
+            # Evento de boton de cierre de ventana
+            for evento in pygame.event.get():
+                if evento.type == pygame.QUIT:
+                    quit()
+
+            #Detectar teclas presionadas
+            k = pygame.key.get_pressed()
+            #Movimiento jugador(es)
+            Dr.move(k, pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN, ventana_x, ventana_y)
+            
+            
+            refresh()
+            
+
